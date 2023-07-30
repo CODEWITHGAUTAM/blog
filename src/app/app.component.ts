c4c349ba0b11e488e8532b24074efa30f1b61efd
CODEWITHGAUTAM <gautamsingh7424@gmail.com>
2023-07-30 22:11:07 +0530

    first blog app with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+      title='blog';
+      getName(name:any)
+      {
+        alert("function called "+name);
+      }
 }
